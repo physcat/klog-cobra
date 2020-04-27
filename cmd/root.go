8a5473f09c3567a33113d003d40fc6d07893eb1c
Mitch <mitch@physicscat.org>
2020-04-27 09:26:24 +0200

    Change to use klog

--- cmd/root.go
+++ cmd/root.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
+	goflags "flag"
+
 	"github.com/spf13/cobra"
+	"k8s.io/klog/v2"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
@@ -25,29 +27,41 @@ Try these two commands:
 go run . 
 go run . -v 5
 `,
-	Run: func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		klog.V(0).InfoS("Message printed at Info level 0")
+		klog.V(2).InfoS("Message printed at Info level 2")
+		klog.V(5).InfoS("Message printed at Info level 5")
+	},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		klog.ErrorS(err, "rootCmd.Execute()")
 		os.Exit(1)
 	}
 }
 
 func init() {
+	klog.InitFlags(nil)
+	rootCmd.Flags().AddGoFlagSet(goflags.CommandLine)
+
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.klog-cobra.yaml)")
 }
 
 func initConfig() {
+	//map command line flags to viper variables.
+	//viper will prefer flags from command line rather than file
+	if err := viper.BindPFlags(rootCmd.Flags()); err != nil {
+		klog.ErrorS(err, "Failed bind flags")
+	}
 	if cfgFile != "" {
 		viper.SetConfigFile(cfgFile)
 	} else {
 		home, err := homedir.Dir()
 		if err != nil {
-			fmt.Println(err)
+			klog.ErrorS(err, "")
 			os.Exit(1)
 		}
 
@@ -57,7 +71,18 @@ func initConfig() {
 
 	viper.AutomaticEnv()
 
+	verbosity := viper.GetString("v")
+	klog.InfoS("Verbosity", "v", verbosity)
+
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		klog.InfoS("Using config", "file", viper.ConfigFileUsed())
+	}
+
+	// Manually update v flag from viper config file
+	if verbosity == "0" {
+		if err := goflags.Set("v", viper.GetString("v")); err != nil {
+			klog.Errorf("%+v", err)
+			return
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.6.3
+	k8s.io/klog/v2 v2.0.0
 )
