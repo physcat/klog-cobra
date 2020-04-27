3c0ecf3cc657ded1e703481172ecdaa74651e731
Mitch <mitch@physicscat.org>
2020-04-27 09:10:22 +0200

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "klog-cobra",
+	Short: "Example how of using klog with a cobra application",
+	Long: `Example how of using klog with a cobra application
+	
+Klog uses golang flags that can sometimes confuse things when using
+cobra. Cobra uses pflags, this shows one way of getting started 
+using both klog and cobra.
+
+Try these two commands:
+go run . 
+go run . -v 5
+`,
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.klog-cobra.yaml)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".klog-cobra")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/physcat/klog-cobra/cmd"
+
+func main() {
+	cmd.Execute()
+}
